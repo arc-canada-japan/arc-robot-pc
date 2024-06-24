eea18bfda0431b363a5c5610aa99cde239608aac
Siméon Capy <simeon.capy@rs.tus.ac.jp>
2024-06-24 13:53:52 +0900

    Basic (text) pub/sub for video (test)

--- src/unity_communication/setup.py
+++ src/unity_communication/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'unity_commmunication'
+package_name = 'unity_communication'
 
 setup(
     name=package_name,
--- src/video_display/setup.py
+++ src/video_display/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'video_sub = video_display.video_subscriber:main',
         ],
     },
 )
