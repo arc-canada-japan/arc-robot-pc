1c05cc0d699b1a3ea65f43e851675faecbbbd72b
Yoshida Lab <contact@yoshidalab.com>
2024-07-10 18:40:52 +0900

    launch file

---
+++ src/robot_control/robot_control/ffmpeg_caller.py
@@ -0,0 +1,45 @@
+import rclpy
+from rclpy.node import Node
+import subprocess
+
+class FfmpegCaller(Node):
+
+    def __init__(self):
+        super().__init__('ffmpeg_caller')
+        # Declare and get parameters
+        self.declare_parameter('ffmpeg_ip', '192.168.11.50')
+        self.declare_parameter('ffmpeg_port', 8080)
+        self.declare_parameter('video_x', 1280)
+        self.declare_parameter('video_y', 720)
+
+        ip = self.get_parameter('ffmpeg_ip').get_parameter_value().string_value
+        port = self.get_parameter('ffmpeg_port').get_parameter_value().integer_value
+        video_x = self.get_parameter('video_x').get_parameter_value().integer_value
+        video_y = self.get_parameter('video_y').get_parameter_value().integer_value
+
+        # Create the command string using an f-string
+        command = f'ffmpeg -s {video_x}x{video_y} -i /dev/video0 -preset ultrafast -tune zerolatency -codec libx264 -f mpegts udp://{ip}:{port}'
+
+        # Split the command string into a list of arguments
+        command_list = command.split()
+
+        # Run the command using subprocess
+        subprocess.run(command_list)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    caller = FfmpegCaller()
+
+    rclpy.spin(caller)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    caller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- src/video_publisher/video_publisher/video_publisher.py
+++ src/video_publisher/video_publisher/video_publisher.py
@@ -1,39 +1,52 @@
+#!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-from std_msgs.msg import String
-
-
-class MinimalPublisher(Node):
-
+class ImagePublisher(Node):
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__("image_publisher")
+        self.bridge = CvBridge()
+        self.cap = cv2.VideoCapture(0)
+        self.pub = self.create_publisher(Image, "/image", 10)
+        self.rgb8pub = self.create_publisher(Image, "/image/rgb", 10)
+        self.bgr8pub = self.create_publisher(Image, "/image/bgr", 10)
+        self.mono8pub = self.create_publisher(Image, "/image/mono", 10)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def run(self):
+        while True:
+            try:
+                r, frame = self.cap.read()
+                if not r:
+                    return
+                self.pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
+                # BGR8
+                self.bgr8pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+                # RGB8
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.rgb8pub.publish(self.bridge.cv2_to_imgmsg(frame_rgb, "rgb8"))
+
+                # MONO8
+                frame_mono = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.mono8pub.publish(self.bridge.cv2_to_imgmsg(frame_mono, "mono8"))
+
+            except CvBridgeError as e:
+                print(e)
+        self.cap.release()
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    ip = ImagePublisher()
+    print("Publishing...")
+    ip.run()
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    ip.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
