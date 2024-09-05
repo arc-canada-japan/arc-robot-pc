fa2faf17ffe07762917527c52c1b6c320548d767
Yoshida Lab <contact@yoshidalab.com>
2024-09-05 18:47:07 +0900

    ZMQ interface + ROS common callback

--- src/communication_interface/communication_interface/nep_server.py
+++ src/communication_interface/communication_interface/nep_server.py
@@ -1,18 +1,41 @@
 import nep
 import netifaces as ni
+import sys
+import ipaddress
+
 
 ### TODO: Implement the NEP server
+# echo $(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 
-def main(args=None):
+def main():
     """
         Main function to start the NEP server.
+        It takes one argument: the IP address of the server or the interface on which IP the server should run.
     """
-    # Get the IP address of the current device
-    ip = ni.ifaddresses('wlp2s0')[ni.AF_INET][0]['addr']
+    ip = sys.argv[1]
 
-    # Create a NEP server
-    server = nep.master(ip)
+    try:
+        ip = ipaddress.ip_address(sys.argv[1]) # Check if the IP address is valid
+    except ValueError:
+        try: # Try to get the IP address from the network interface
+            ip = ipaddress.ip_address(ni.ifaddresses(sys.argv[1])[ni.AF_INET][0]['addr']) 
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
 
-    # Start the server
-    server.run()
+    try:
+        # Create a NEP server
+        server = nep.master(str(ip))
+        # Start the server
+        server.run()
+    except KeyboardInterrupt:
+        print("Server stopped.")
+        server.stop()
+        sys.exit(0)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
 
+
+if __name__ == '__main__':
+    main()
